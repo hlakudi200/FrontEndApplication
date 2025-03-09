729b512b471685ba94ab49b98669dd0b7e5b5e5b
hlakudi200 <tlhomphohlakudi@gmail.com>
2025-03-09 21:51:52 +0200

    Complete Client Card Component

--- src/app/trainer/dashboard/page.tsx
+++ src/app/trainer/dashboard/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import styles from './styles.module.css'
+import ClientCard from '@/_components/client-card/client-card';
 
 function TrainerDashBoard() {
   return (
-    <div>page</div>
+    <div id='IouterContainer' className={styles.outerContainer}>
+        <div id='IleftContainer' className={styles.leftContainerr}>
+          <ClientCard clientName='Rapudi Hlakudi' clientEmail='rapudi@virgin.com' clientContactNo='+27 60 670 4536' imageSrc='/images/user.png'/>
+          <ClientCard clientName='Rapudi Hlakudi' clientEmail='rapudi@virgin.com' clientContactNo='+27 60 670 4536' imageSrc='/images/user.png'/>
+          <ClientCard clientName='Rapudi Hlakudi' clientEmail='rapudi@virgin.com' clientContactNo='+27 60 670 4536' imageSrc='/images/user.png'/>
+          <ClientCard clientName='Rapudi Hlakudi' clientEmail='rapudi@virgin.com' clientContactNo='+27 60 670 4536' imageSrc='/images/user.png'/>
+          <ClientCard clientName='Rapudi Hlakudi' clientEmail='rapudi@virgin.com' clientContactNo='+27 60 670 4536' imageSrc='/images/user.png'/>
+          <ClientCard clientName='Rapudi Hlakudi' clientEmail='rapudi@virgin.com' clientContactNo='+27 60 670 4536' imageSrc='/images/user.png'/>
+          <ClientCard clientName='Rapudi Hlakudi' clientEmail='rapudi@virgin.com' clientContactNo='+27 60 670 4536' imageSrc='/images/user.png'/>
+        </div>
+        <div id='ImiddleContainer' className={styles.middleContainer}></div>
+        <div id='IrightContainer' className={styles.rightContainer}></div>
+    </div>
+      
   )
 }
 
-export default TrainerDashBoard()
+export default TrainerDashBoard;
